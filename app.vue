d0d0d6472301972551eac2ff8e06eebe3a18f4e6
Alvaro <alvaro.saburido@gmail.com>
2021-10-27 10:01:41 +0200

    feat: connect to metamask wallet

--- app.vue
+++ app.vue
@@ -1,3 +1,60 @@
+<script setup>
+const currentAccount = ref('')
+function checkIfWalletIsConnected() {
+  /*
+   * First make sure we have access to window.ethereum
+   */
+  const { ethereum } = window
+
+  if (!ethereum) {
+    console.log('Make sure you have metamask!')
+    return
+  } else {
+    console.log('We have the ethereum object', ethereum)
+  }
+}
+
+async function checkAuthorization() {
+  try {
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+    console.log({ accounts })
+
+    if (accounts.length !== 0) {
+      const account = accounts[0]
+      console.log('Found an authorized account:', account)
+      currentAccount.value = account
+    } else {
+      console.log('No authorized account found')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function connectWallet() {
+  try {
+    const { ethereum } = window
+
+    if (!ethereum) {
+      alert('Get MetaMask!')
+      return
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+
+    console.log('Connected', accounts[0])
+    currentAccount.value = accounts[0]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(async () => {
+  checkIfWalletIsConnected()
+  await checkAuthorization()
+})
+</script>
 <template>
   <div
     class="
@@ -15,7 +72,16 @@
     <h1 class="font-display text-secondary-400 text-6xl mb-8">
       🍺 Beer Portal
     </h1>
-    <p class="text-2xl font-sans">Send a FE to the metaverse with your 🍺</p>
+    <p class="text-2xl font-sans mb-8">
+      Send a FE to the metaverse with your 🍺
+    </p>
+    <button
+      v-if="!currentAccount"
+      class="bg-secondary-400 py-2.5 px-1.5 font-bold rounded flex items-center"
+      @click="connectWallet"
+    >
+      <img src="/metamask-fox.svg" class="w-4 h-4 mr-2" /> Connect Wallet
+    </button>
     <footer
       class="
         fixed
@@ -29,8 +95,8 @@
       "
     >
       Made by
-      <a href="https://twitter.com/alvaro_code" class="text-secondary-400 mx-2"
-        >Alvaro</a
+      <a href="https://twitter.com/alvaro_code" class="text-secondary-400 mx-2">
+        Alvaro</a
       >
       with
       <a
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import Icons from 'unplugin-icons/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    Icons({
+      /* options */
+    }),
+  ],
+})
